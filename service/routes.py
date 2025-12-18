a241e011dd4796a8682bb6e30e7f5d2c218bb75f
pdeghesel <pascale.degheselle@yahoo.fr>
2025-12-18 23:16:54 +0100

    Update routes.py (lint)

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -74,15 +75,15 @@ def list_accounts():
     # create a list of serialize() accounts
     account_list = [account.serialize() for account in accounts]
 
-    # log the number of accounts being returned in the list 
+    # log the number of accounts being returned in the list
     app.logger.info("Returning [%s] accounts", len(account_list))
     # return the list with a return code of status.HTTP_200_OK
     return jsonify(account_list), status.HTTP_200_OK
 
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -97,8 +98,8 @@ def get_accounts(account_id):
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
-    # return the serialize() version of the account 
+
+    # return the serialize() version of the account
     # with a return code of status.HTTP_200_OK
     return account.serialize(), status.HTTP_200_OK
 
